1e948f8a527f52d7122470dcd5a11422bdb0927e
Kameron Keller <mrkameronkeller@gmail.com>
2022-12-30 12:39:02 -0800

    Setup prompts for Canvas URL

---
+++ config_setup.py
@@ -0,0 +1,8 @@
+import configparser
+from simple_term_menu import TerminalMenu
+from commandline_printer import CommandLinePrinter as printer
+
+config = configparser.ConfigParser()
+
+printer.print_divider("Canvas Setup")
+canvas_api_url = input("Paste your Canvas URL (e.g. https://canvas.oregonstate.edu/): ")
