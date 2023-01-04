9bc897bc7cb72afbe94ee4d814a66954fb7b27ee
Kameron Keller <mrkameronkeller@gmail.com>
2023-01-04 13:16:27 -0800

    Add update config method

--- config_manager.py
+++ config_manager.py
@@ -14,4 +14,8 @@ class ConfigManager:
 		self.config_parser[name] = {}
 		self.write()
 
+	def update_config(section, attribute, value):
+		config_parser[section][attribute] = value
+		self.write()
+
 
