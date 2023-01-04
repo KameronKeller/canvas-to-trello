7d54ef4e6c647d4a00a19349d3dfada744e749ff
Kameron Keller <mrkameronkeller@gmail.com>
2023-01-04 12:31:23 -0800

    Prompt for trello info and store in config

--- config_setup.py
+++ config_setup.py
@@ -9,3 +9,11 @@ canvas_api_url = input("Paste your Canvas URL (e.g. https://canvas.oregonstate.e
 config['canvas'] = {'url': canvas_api_url,
 					'api_key': ""}
 
+
+printer.print_divider("Trello Setup")
+input("Press enter to get your Trello API Key.")
+webbrowser.get().open("https://trello.com/app-key")
+trello_api_key = input("Paste the provided Trello API key here: ")
+trello_api_token = input("Generate a token and paste it here: ")
+config['trello'] = {'api_key': trello_api_key,
+                    'api_token': trello_api_token}
