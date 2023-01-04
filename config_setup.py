a77c776b99dc955ecf77305587b9cd0e4471ec6b
Kameron Keller <mrkameronkeller@gmail.com>
2023-01-04 13:27:23 -0800

    Refactor code into trello manager

--- config_setup.py
+++ config_setup.py
@@ -1,9 +1,10 @@
 # import configparser
-import webbrowser
+# import webbrowser
 from simple_term_menu import TerminalMenu
 from commandline_printer import CommandLinePrinter as printer
 from config_manager import ConfigManager
 from canvas_manager import CanvasManager
+from trello_manager import TrelloManager
 
 # config_parser = configparser.ConfigParser()
 
@@ -11,25 +12,14 @@ CONFIG_FILE = 'config.ini'
 CONFIG_SECTIONS = ['canvas', 'trello', 'trello_board', 'trello_list']
 
 config_manager = ConfigManager(CONFIG_FILE)
+canvas_manager = CanvasManager(config_manager)
+trello_manager = TrelloManager(config_manager)
 
 for section in CONFIG_SECTIONS:
 	config_manager.add_config_section(section)
 
-canvas_manager = CanvasManager(config_manager)
-
 canvas_manager.interactive_setup()
-
-
-# printer.print_divider("Trello Setup")
-# input("Press enter to get your Trello API Key.")
-# webbrowser.get().open("https://trello.com/app-key")
-# trello_api_key = input("Paste the provided Trello API key here: ")
-# trello_api_token = input("Generate a token and paste it here: ")
-# config['trello'] = {'api_key': trello_api_key,
-#                     'api_token': trello_api_token}
-
-# with open('config.ini', 'w') as configfile:
-#   config.write(configfile)
+trello_manager.interactive_setup()
 
 # trello_client = TrelloClient(
 #     api_key=trello_api_key,
