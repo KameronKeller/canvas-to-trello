633eff8eca298e825a7d1be50ae2abfb2b9a0623
Kameron Keller <mrkameronkeller@gmail.com>
2023-01-04 14:21:01 -0800

    Remove comments, and uncomment web browser from testing

--- trello_manager.py
+++ trello_manager.py
@@ -12,10 +12,6 @@ class TrelloManager:
 	def interactive_setup(self):
 		printer.print_divider("Trello Setup")
 		trello_api_key, trello_api_token = self.request_api_info()
-		# input("Press enter to get your Trello API Key.")
-		# webbrowser.get().open("https://trello.com/app-key")
-		# trello_api_key = input("Paste the provided Trello API key here: ")
-		# trello_api_token = input("Generate a token and paste it here: ")
 		self.config_manager.update_config('trello', 'api_key', trello_api_key)
 		self.config_manager.update_config('trello', 'api_token', trello_api_token)
 		self.trello_client = TrelloClient(api_key=trello_api_key, api_secret=trello_api_token)
@@ -24,7 +20,7 @@ class TrelloManager:
 
 	def request_api_info(self):
 		input("Press enter to get your Trello API Key.")
-		# webbrowser.get().open("https://trello.com/app-key")
+		webbrowser.get().open("https://trello.com/app-key")
 		trello_api_key = input("Paste the provided Trello API key here: ")
 		trello_api_token = input("Generate a token and paste it here: ")
 		return trello_api_key, trello_api_token
