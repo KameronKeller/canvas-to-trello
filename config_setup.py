c479d37c0314bb8b9ac8584367092d3da032de57
Kameron Keller <mrkameronkeller@gmail.com>
2023-01-04 13:21:10 -0800

    Refactor canvas into manager class

--- config_setup.py
+++ config_setup.py
@@ -1,23 +1,37 @@
-import configparser
+# import configparser
 import webbrowser
 from simple_term_menu import TerminalMenu
 from commandline_printer import CommandLinePrinter as printer
+from config_manager import ConfigManager
+from canvas_manager import CanvasManager
 
-config = configparser.ConfigParser()
+# config_parser = configparser.ConfigParser()
 
-printer.print_divider("Canvas Setup")
-canvas_api_url = input("Paste your Canvas URL (e.g. https://canvas.oregonstate.edu/): ")
-config['canvas'] = {'url': canvas_api_url,
-					'api_key': ""}
+CONFIG_FILE = 'config.ini'
+CONFIG_SECTIONS = ['canvas', 'trello', 'trello_board', 'trello_list']
+
+config_manager = ConfigManager(CONFIG_FILE)
+
+for section in CONFIG_SECTIONS:
+	config_manager.add_config_section(section)
+
+canvas_manager = CanvasManager(config_manager)
+
+canvas_manager.interactive_setup()
 
 
-printer.print_divider("Trello Setup")
-input("Press enter to get your Trello API Key.")
-webbrowser.get().open("https://trello.com/app-key")
-trello_api_key = input("Paste the provided Trello API key here: ")
-trello_api_token = input("Generate a token and paste it here: ")
-config['trello'] = {'api_key': trello_api_key,
-                    'api_token': trello_api_token}
+# printer.print_divider("Trello Setup")
+# input("Press enter to get your Trello API Key.")
+# webbrowser.get().open("https://trello.com/app-key")
+# trello_api_key = input("Paste the provided Trello API key here: ")
+# trello_api_token = input("Generate a token and paste it here: ")
+# config['trello'] = {'api_key': trello_api_key,
+#                     'api_token': trello_api_token}
 
-with open('config.ini', 'w') as configfile:
-  config.write(configfile)
+# with open('config.ini', 'w') as configfile:
+#   config.write(configfile)
+
+# trello_client = TrelloClient(
+#     api_key=trello_api_key,
+#     api_secret=trello_api_token
+# )
