bb9cc7e59234473a13e329fa032508ccafdb2a49
Kameron Keller <mrkameronkeller@gmail.com>
2023-01-05 11:22:48 -0800

    Import trello manager

--- update_trello.py
+++ update_trello.py
@@ -1,6 +1,7 @@
 from canvas_manager import CanvasManager
 from config_manager import ConfigManager
 from database_manager import DatabaseManager
+from trello_manager import TrelloManager
 
 config_manager = ConfigManager()
 canvas_manager = CanvasManager(config_manager)
@@ -9,4 +10,4 @@ database_manager = DatabaseManager(canvas_manager, trello_manager)
 
 database_manager.update_database()
 
-database_manager.send_assignments_to_trello()
+# database_manager.send_assignments_to_trello()
