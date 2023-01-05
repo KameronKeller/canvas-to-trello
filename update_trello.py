cb40733bf81e6e3029c6c76d6de8b1eb8bbe7cec
Kameron Keller <mrkameronkeller@gmail.com>
2023-01-05 08:49:59 -0800

    Import canvas assignments to database and send assignments to trello

---
+++ update_trello.py
@@ -0,0 +1,12 @@
+from canvas_manager import CanvasManager
+from config_manager import ConfigManager
+from database_manager import DatabaseManager
+
+config_manager = ConfigManager()
+canvas_manager = CanvasManager(config_manager)
+trello_manager = TrelloManager(config_manager)
+database_manager = DatabaseManager(canvas_manager, trello_manager)
+
+database_manager.update_database()
+
+database_manager.send_assignments_to_trello()
