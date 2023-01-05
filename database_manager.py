f209e14c2e988979bf1d43834c2e2567bd9904db
Kameron Keller <mrkameronkeller@gmail.com>
2023-01-05 08:56:40 -0800

    Create constructor

--- database_manager.py
+++ database_manager.py
@@ -2,4 +2,9 @@ import sqlite3
 
 class DatabaseManager:
 
+	def __init__(self, canvas_manager, trello_manager, database_file='assignments.db'):
+		self.canvas_manager = canvas_manager
+		self.trello_manager = trello_manager
+		self.database_file = database_file
+
 
