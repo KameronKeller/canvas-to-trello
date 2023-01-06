c3196b7ca136cec77c0ba47b42193e171356872e
Kameron Keller <mrkameronkeller@gmail.com>
2023-01-06 13:27:26 -0800

    Add update trello and echo statements

--- setup.sh
+++ setup.sh
@@ -11,10 +11,15 @@ python3 -m venv env
 source env/bin/activate
 
 # Install required python packages
+echo "========== Install Required Packages =========="
 pip install -r requirements.txt
 
 # Run config setup to create config.ini to store API information
 python3 config_setup.py
+
+# Refresh Trello with the latest assignments from canvas
+echo "Copying assignments to Trello. This may take a few minutes..."
+python3 update_trello.py
 
 # Deactivate the Python virtual environment
 deactivate
