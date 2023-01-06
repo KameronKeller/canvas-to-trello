e7d4b6aeb16d66c645e1fc7a18163605ecc595f0
Kameron Keller <mrkameronkeller@gmail.com>
2023-01-06 13:44:09 -0800

    Move logic to function

--- sync.py
+++ sync.py
@@ -3,10 +3,11 @@ from config_manager import ConfigManager
 from database_manager import DatabaseManager
 from trello_manager import TrelloManager
 
-config_manager = ConfigManager()
-canvas_manager = CanvasManager(config_manager)
-trello_manager = TrelloManager(config_manager)
-database_manager = DatabaseManager(canvas_manager, trello_manager)
+def sync():
+	config_manager = ConfigManager()
+	canvas_manager = CanvasManager(config_manager)
+	trello_manager = TrelloManager(config_manager)
+	database_manager = DatabaseManager(canvas_manager, trello_manager)
 
-database_manager.update_database()
-database_manager.send_assignments_to_trello()
+	database_manager.update_database()
+	database_manager.send_assignments_to_trello()
